aac6b3733ac6389eb222c323ed432a01efb9d528
Vadim <isako-vadim@yandex.ru>
2025-09-18 20:30:59 +0500

    REFACTOR

--- src/main/kotlin/ru/pachan/writer_kotlin/model/Notification.kt
+++ src/main/kotlin/ru/pachan/writer_kotlin/model/Notification.kt
@@ -1,7 +1,13 @@
 package ru.pachan.writer_kotlin.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
 
 
 @Entity
@@ -22,6 +28,6 @@ class Notification(
     )
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "notifications_seq")
     @Column(name = "notification_id")
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    @field:JsonProperty(access = JsonProperty.Access.READ_ONLY)
     val id: Long = 0,
 )
